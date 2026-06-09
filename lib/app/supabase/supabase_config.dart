1a4571aa705366d0c72b45d6058be5d95132fd2e
KerolosKamal0 <kerolos.kamal99@gmail.com>
2026-06-09 22:33:08 +0300

    fixed

--- lib/app/supabase/supabase_config.dart
+++ lib/app/supabase/supabase_config.dart
@@ -1,13 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 abstract final class SupabaseConfig {
-  static final url = String.fromEnvironment(
-    'SUPABASE_URL',
-    defaultValue: dotenv.get('SUPABASE_URL'),
-  );
+  static String get url => dotenv.get('SUPABASE_URL');
 
-  static final publishableKey = String.fromEnvironment(
-    'SUPABASE_ANON_KEY',
-    defaultValue: dotenv.get('SUPABASE_ANON_KEY'),
-  );
+  static String get publishableKey => dotenv.get('SUPABASE_ANON_KEY');
 }
